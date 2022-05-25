6210b461e8b889581e7c9bff73097d6e10faf41f
MoVaCA <105380737+MoVaCA@users.noreply.github.com>
2022-05-25 11:44:37 -0700

    style a botones de export

--- html/Reportes/Inventario/main.js
+++ html/Reportes/Inventario/main.js
@@ -41,11 +41,60 @@ function LoadAllSecuritySeals() {
 	        { title: "Status" },
             { title: "Fecha Recibido" }
         ],
-        buttons: [ 'copy', 'csv', 'excel', 'pdf', 'print' ],
+        buttons: [ 
+            {
+                extend:    'copyHtml5',
+                text:      '<i class="fa fa-clipboard"></i>Copiar',
+                title:'Titulo de tabla copiada',
+                titleAttr: 'Copiar',
+                className: 'btn btn-app export barras',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 3, 4 , 5, 6, 7, 8]
+                }  
+            },
+            {
+                extend:    'pdfHtml5',
+                        text:      '<i class="fa fa-file-pdf-o"></i>PDF',
+                        title:'Titulo de tabla en pdf',
+                        titleAttr: 'PDF',
+                        className: 'btn btn-app export pdf',
+                        exportOptions: {
+                            columns: [ 0, 1, 2, 3, 4 , 5, 6, 7, 8]
+            }
+        },
+        
+        {
+            extend:    'excelHtml5',
+            text:      '<i class="fa fa-file-excel-o"></i>Excel',
+            title:'Titulo de tabla en excel',
+            titleAttr: 'Excel',
+            className: 'btn btn-app export excel',
+            exportOptions: {
+                columns: [ 0, 1, 2, 3, 4 , 5, 6, 7, 8]
+            },
+        },
 
-        
-        
-        });
+        {
+            extend:    'csvHtml5',
+                        text:      '<i class="fa fa-file-text-o"></i>CSV',
+                        title:'Titulo de tabla en CSV',
+                        titleAttr: 'CSV',
+                        className: 'btn btn-app export csv',
+                        exportOptions: {
+                            columns: [ 0, 1, 2, 3, 4 , 5, 6, 7, 8]
+        }
+    },
+    {
+        extend:    'print',
+        text:      '<i class="fa fa-print"></i>Imprimir',
+        title:'Titulo de tabla en impresion',
+        titleAttr: 'Imprimir',
+        className: 'btn btn-app export imprimir',
+        exportOptions: {
+            columns: [ 0, 1 ]
+        }
+    },],
+ });
             
         }
       });
